c6b266f40ea1b74ca7972cd2a8170245417414b7
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-05 01:59:38 -0700

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "eflow", description: "Flow machines",
+                 version: :head, keywords: ["flow","machine"], 
+                 maintainers: [[name: "Yurii Rashkovskii", 
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/eflow"]])
+    
